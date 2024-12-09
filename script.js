88c37403a69f89137f16d545085a56c4a84a90c1
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-12-09 21:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-//your JS code here. If required.
 let ageInput = document.querySelector("#age");
 let nameInput = document.getElementById("name");
 let submitButton = document.querySelector("#btn");
 let ageValidate = document.querySelector("#ageValidate");
 let nameValidate = document.querySelector("#nameValidate");
 
-
 ageValidate.style.display = "none";
 nameValidate.style.display = "none";
 
-
 submitButton.addEventListener("click", Validation);
-function Validation(e){
-    e.preventDefault();
-    if (ageInput.value.length == 0) {
-      ageValidate.style.display = "block";
-    }
-     if (nameInput.value.length == 0) {
-      nameValidate.style.display = "block";
-    }
-    else {
-      Result();
-    }
 
+function Validation(e) {
+  let isValid = true;
+  e.preventDefault();
+  if ((ageInput.value.trim() ==="")) {
+    ageValidate.style.display = "block";
+    isValid = false;
+  } else {
+    ageValidate.style.display = "none";
+  }
+
+  if ((nameInput.value.trim() === "")) {
+    nameValidate.style.display = "block";
+    isValid = false;
+  } else {
+    nameValidate.style.display = "none";
+  }
+
+  if (isValid) {
+    Result();
+  }
 }
 
-
-
-
-function Result(){
+function Result() {
   let promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (ageInput.value > 18) {
+      
+      if (parseInt(ageInput.value) > 18) {
         resolve(`Welcome, ${nameInput.value} You can vote.`);
       } else {
         reject(`Oh sorry ${nameInput.value} You aren't old enough.`);
       }
-    }, 40);
+    }, 4000);
   });
 
   promise
